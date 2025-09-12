15121449dec63d5351a485e33e78b24759741f6b
Jonathan <dos.santos.2k23@gmail.com>
2025-09-11 22:03:34 -0300

    feat: set Dashboard Login route at main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import DashboardLogin from "./pages/Dashboard/Login/index.jsx";
 
-createRoot(document.getElementById('root')).render(
+const browserRouter = createBrowserRouter(createRoutesFromElements(
+  <Route path='/dashboard'>
+    <Route path='login' element={<DashboardLogin />} />
+  </Route>
+))
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={browserRouter}/>
+  </StrictMode>
+);
