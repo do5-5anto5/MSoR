7de78cdd7f882e4ecac3a22a79efb75e0ab6df44
Jonathan <dos.santos.2k23@gmail.com>
2025-09-17 07:47:33 -0300

    feat: set user create and edit routes at main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Dashboard from "./pages/Dashboard/index.jsx";
-import Layout from "./pages/Dashboard/Login/components/Layout/Layout.jsx";
-import DashboardLogin from "./pages/Dashboard/Login/DashoardLogin.jsx";
+import Dashboard from "./pages/dashboard/index.jsx";
+import Layout from "./pages/dashboard/login/components/layout/Layout.jsx";
+import CreateUser from "./pages/dashboard/users/create/Create.jsx";
+import DashboardLogin from "./pages/Dashboard/login/DashoardLogin.jsx";
+import EditUser from "./pages/dashboard/users/edit/Edit.jsx";
 
 const browserRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/dashboard" element={<Layout />}>
         <Route index element={<Dashboard />} />
+        <Route path="users/create" element={<CreateUser />} />
+        <Route path="users/edit/:userId" element={<EditUser />} />
         <Route path="products" element={null} />
         <Route path="categories" element={null} />
       </Route>
