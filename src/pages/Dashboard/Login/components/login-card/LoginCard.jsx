eb065d0c626badccd528cf1ae7d1fc9c5039a615
Jonathan <dos.santos.2k23@gmail.com>
2025-09-17 16:03:58 -0300

    refactor: adapt LoginCard form implementing InputDefault

--- src/pages/Dashboard/Login/components/login-card/LoginCard.jsx
+++ src/pages/Dashboard/Login/components/login-card/LoginCard.jsx
@@ -2,49 +2,47 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
+import InputDefault from "../../../components/input-default/InputDefault";
 
 const schema = yup.object().shape({
-  email: yup.string().email('Email inválido').required('Preencha o campo email'),
-  password: yup.string().required('Preencha o campo email'),
+  email: yup.string().email("Email inválido").required("Email é obrigatório"),
+  password: yup
+    .string()
+    .min(4, "Senha deve ter no mínimo 4 caracteres")
+    .required("Senha é obrigatório"),
 });
 
 export default function LoginCard() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const formObject = useForm({
     resolver: yupResolver(schema),
   });
 
   function onSubmit() {
-    navigate('/dashboard')
+    navigate("/dashboard");
   }
-
 
   return (
     <div className="bg-white px-6 py-8 rounded-lg shadow-md w-80">
       <form onSubmit={formObject.handleSubmit(onSubmit)}>
-        <div className="flex flex-col gap-y-1">
-          <label htmlFor="email">Email</label>
-          <input
-            type="text"
-            id="email"
-            className="border outline-none px-3 py-1 rounded-lg"
-            placeholder="Digite seu email"
-            {...formObject.register("email")}
-          />
-        </div>
-        <span className="text-red-500">{formObject.formState.errors.email?.message}</span>
-        <div className="flex flex-col gap-y-1 mt-10">
-          <label htmlFor="password">Senha</label>
-          <input
-            type="password"
-            id="password"
-            className="border outline-none px-3 py-1 rounded-lg"
-            placeholder="Digite sua senha"
-            {...formObject.register("password")}
-            />
-            <span className="text-red-500">{formObject.formState.errors.password?.message}</span>
-        </div>
+        <InputDefault
+          id={"email"}
+          label={"Email"}
+          type={"text"}
+          placeholder={"Digite o email"}
+          formObject={formObject.register("email")}
+          errorMessage={formObject.formState.errors.email?.message}
+        />
+        <InputDefault
+          id={"password"}
+          label={"Senha"}
+          type={"password"}
+          placeholder={"Digite a senha"}
+          formObject={formObject.register("password")}
+          errorMessage={formObject.formState.errors.password?.message}
+        />
+
         <button
           type="submit"
           className="mt-7 bg-blue-300 w-full py-2 rounded-lg text-white"
