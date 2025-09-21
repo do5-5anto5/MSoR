6649e20c1875ab4994fcf7ed865e700c31c45d6a
Jonathan <dos.santos.2k23@gmail.com>
2025-09-21 15:26:40 -0300

    refactor: make form validation error messages more polite

--- src/pages/Dashboard/products/components/schema.js
+++ src/pages/Dashboard/products/components/schema.js
@@ -3,9 +3,9 @@ import * as yup from "yup";
 export const schema = yup.object().shape({
   name: yup
     .string()
-    .required("Campo obrigatório")
+    .required("Campo necessário")
     .min(3, "Mínimo de 3 caracteres"),
-    price: yup.string().required("Campo obrigatório"),
+    price: yup.string().required("Campo necessário"),
     stock: yup.string(),
-    category: yup.string().required("Campo obrigatório"),
+    category: yup.string().required("Campo necessário"),
 });
