1fefd2ade87b8f0ba0436f5b7f3f2cc54d286198
Jonathan <dos.santos.2k23@gmail.com>
2025-09-22 21:55:06 -0300

    feat: create ProductCard.jsx component

---
+++ src/pages/home/components/card/product/ProductCard.jsx
@@ -0,0 +1,51 @@
+import PropTypes from "prop-types";
+import { IoCartOutline } from "react-icons/io5";
+
+export default function ProductCard(props) {
+  return (
+    <div className="rounded-2xl border border-gray-200 shadow-lg">
+      <div className="h-36">
+        <img
+          src={props.product.image}
+          className="w-full h-full object-cover rounded-2xl "
+          alt="Nome do Produto"
+        />
+      </div>
+      <div className="px-4 pt-6 pb-4">
+        <h2 className="text-lg text-gray-800 font-medium">
+          {props.product.name}
+        </h2>
+        <span className="text-gray-400">
+          Restam {props.product.stock} no estoque
+        </span>
+        <button
+          className="
+                  bg-primary text-white
+                  rounded-2xl 
+                  w-full flex 
+                  justify-center items-center gap-x-3
+                  mt-6 py-2
+                  "
+        >
+          <IoCartOutline className="text-3xl text-white" />
+          <span className="text-lg font-medium">
+            {Intl.NumberFormat("pt-BR", {
+              currency: "BRL",
+              style: "currency",
+            }).format(props.product.price)}
+          </span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    stock: PropTypes.number.isRequired,
+  }),
+};
