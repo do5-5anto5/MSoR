9a3332ff9c91dc694b6d8355d580621622bfe3c0
Jonathan <dos.santos.2k23@gmail.com>
2025-09-25 15:34:20 -0300

    feat: dynamize Cart total price display

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,6 +1,9 @@
 import { FaMinus, FaPlus, FaTrash } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 export default function Cart() {
+  const [total, setTotal] = useState(0);
+
   const items = [
     {
       productId: 1,
@@ -28,77 +31,98 @@ export default function Cart() {
     },
   ];
 
+  useEffect(() => {
+    let t = 0;
+    items.forEach((item) => {
+      t += item.subTotal;
+    });
+    setTotal(t);
+  }, []);
+
   return (
-    <div>
-      <main className="min-h-screen px-12 pt-10">
-        <div className="overflow-x-auto rounded-2xl">
-          <table className="table">
-            <thead className="bg-primary text-white bg-gradient-to-r from-blue-500 to-blue-700">
-              <tr className="font-medium text-lg">
-                <th className="w-1/6">Produto</th>
-                <th className="w-1/6">Nome</th>
-                <th className="w-1/6">Preço</th>
-                <th className="w-1/6">Quantidade</th>
-                <th className="w-1/6">Subtotal</th>
-                <th className="w-1/6"></th>
-              </tr>
-            </thead>
-            <tbody>
-              {items.map((item, index) => (
-                <tr
-                  key={item.productId}
-                  className={`${
-                    index % 2 === 0 ? "bg-blue-100" : "bg-blue-50"
-                  }`}
-                >
-                  <td>
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="h-20 w-20 rounded-full object-cover"
+    <main className="min-h-screen px-12 pt-10">
+      <span className="text-2xl text-right font-medium block mb-6">
+        Total:{" "}
+        {Intl.NumberFormat("pt-BR", {
+          currency: "BRL",
+          style: "currency",
+        }).format(total)}
+      </span>
+      <div className="overflow-x-auto rounded-2xl">
+        <table className="table">
+          <thead className="bg-primary text-white bg-gradient-to-r from-blue-500 to-blue-700">
+            <tr className="font-medium text-lg">
+              <th className="w-1/6">Produto</th>
+              <th className="w-1/6">Nome</th>
+              <th className="w-1/6">Preço</th>
+              <th className="w-1/6">Quantidade</th>
+              <th className="w-1/6">Subtotal</th>
+              <th className="w-1/6"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr
+                key={item.productId}
+                className={`${index % 2 === 0 ? "bg-blue-100" : "bg-blue-50"}`}
+              >
+                <td>
+                  <img
+                    src={item.image}
+                    alt={item.name}
+                    className="h-20 w-20 rounded-full object-cover"
+                  />
+                </td>
+                <td>{item.name}</td>
+                <td>
+                  {Intl.NumberFormat("pt-BR", {
+                    currency: "BRL",
+                    style: "currency",
+                  }).format(item.price)}
+                </td>
+                <td>
+                  <div className="flex items-center gap-x-2">
+                    <button>
+                      <FaPlus className="text-xs font-light text-blue-500" />
+                    </button>
+                    <input
+                      type="text"
+                      value={item.qty}
+                      className="border-2 border-gray-300 rounded-lg p-2 w-10 text-center font-medium"
                     />
-                  </td>
-                  <td>{item.name}</td>
-                  <td>
-                    {Intl.NumberFormat("pt-BR", {
-                      currency: "BRL",
-                      style: "currency",
-                    }).format(item.price)}
-                  </td>
-                  <td>
-                    <div className="flex items-center gap-x-2">
-                      <button>
-                        <FaPlus className="text-xs font-light text-blue-500" />
-                      </button>
-                      <input
-                        type="text"
-                        value={item.qty}
-                        className="border-2 border-gray-300 rounded-lg p-2 w-10 text-center font-medium"
-                      />
-                      <button>
-                        <FaMinus className="text-xs font-light text-blue-500" />
-                      </button>
-                    </div>
-                  </td>
-                  <td>
-                    {Intl.NumberFormat("pt-BR", {
-                      currency: "BRL",
-                      style: "currency",
-                    }).format(item.subTotal)}
-                  </td>
-                  <td>
-                    <div className="flex justify-center">
-                      <button>
-                        <FaTrash className="text-red-400" />
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </main>
-    </div>
+                    <button>
+                      <FaMinus className="text-xs font-light text-blue-500" />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  {Intl.NumberFormat("pt-BR", {
+                    currency: "BRL",
+                    style: "currency",
+                  }).format(item.subTotal)}
+                </td>
+                <td>
+                  <div className="flex justify-center">
+                    <button>
+                      <FaTrash className="text-red-400" />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex justify-end mt-6">
+        <button
+          className="bg-primary 
+            font-medium text-white
+            py-3 px-6 rounded-xl
+          "
+        >
+          Finalizar compra
+        </button>
+      </div>
+    </main>
   );
 }
